1884409c262c15f9a479f5996e5a1c26f7ca4f16
Manoj Waikar <manoj.waikar@intelliworkspace.com>
2014-06-06 23:36:36 +0530

    show errors in a separate panel.

--- app/scripts/controllers/weather.js
+++ app/scripts/controllers/weather.js
@@ -6,23 +6,40 @@ var WeatherCtrl = function ($scope, openWeather) {
   console.log(this);
 
   $scope.data = {
-    delimiterSeparatedCities: "Pune,India; London,UK",
+    delimiterSeparatedCities: "",
     cities: [],
-    weathers: []
+    weathers: [],
+    isError: false,
+    errors: []
+  };
+
+  $scope.isModelValid = function () {
+    return $scope.data.delimiterSeparatedCities.trim() !== "";
   };
 
   $scope.showWeather = function (data) {
     $scope.data.weathers.push(data);
   };
 
+  $scope.showError = function (data) {
+    $scope.data.isError = true;
+    if (data.cod === "404") {
+      $scope.data.errors.push("Could not find data.");
+    } else {
+      $scope.data.errors.push(data);
+    }
+  };
+
   $scope.getWeather = function () {
+    $scope.data.isError = false;
+    $scope.data.errors = [];
     $scope.data.weathers = [];
     $scope.data.cities = $scope.data.delimiterSeparatedCities.split(";");
+
     _.each($scope.data.cities, function (city) {
-      openWeather.getWeather(city.trim(), $scope.showWeather);
+      openWeather.getWeather(city.trim(), $scope.showWeather, $scope.showError);
     });
   };
 };
 
-//WeatherCtrl.$inject = ['$scope', 'openWeather'];
 app.controller('WeatherCtrl', WeatherCtrl);
