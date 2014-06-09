956519c1d4edaf0bfed1b6e03be65bf569a93c3b
Manoj Waikar <manoj.waikar@intelliworkspace.com>
2014-06-10 02:27:31 +0530

    Write unit tests for weather controller.

--- test/spec/controllers/weather.js
+++ test/spec/controllers/weather.js
@@ -2,21 +2,56 @@
 
 describe('Controller: WeatherCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('weatherappApp'));
+	// load the controller's module
+	beforeEach(module('weatherappApp'));
 
-  var WeatherCtrl,
-    scope;
+	var WeatherCtrl,
+		scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    WeatherCtrl = $controller('WeatherCtrl', {
-      $scope: scope
-    });
-  }));
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function ($controller, $rootScope) {
+		scope = $rootScope.$new();
+		WeatherCtrl = $controller('WeatherCtrl', {
+			$scope: scope
+		});
+	}));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+	it('should have data in its initial state in scope', function () {
+		expect(scope.data.isError).toBe(false);
+		expect(scope.data.delimiterSeparatedCities).toBe("");
+		expect(scope.data.cities).toEqual([]);
+		expect(scope.data.weathers).toEqual([]);
+		expect(scope.data.errors).toEqual([]);
+	});
+
+	it('should show model as invalid if the textbox is empty', function () {
+		expect(scope.isModelValid()).toEqual(false);
+	});
+
+	it('should show model as valid if the textbox is not empty', function () {
+		scope.data.delimiterSeparatedCities = "pune";
+		expect(scope.isModelValid()).toEqual(true);
+	});
+
+	it('should return the model to its initial state when initModel is called', function () {
+		scope.data.isError = true;
+		scope.data.errors = [1, 2, 3];
+		scope.data.weathers = [4, 5, 6];
+
+		scope.initModel();
+		expect(scope.data.isError).toBe(false);
+		expect(scope.data.weathers).toEqual([]);
+		expect(scope.data.errors).toEqual([]);
+	});
+
+	it('should populate weather data on successful call of openWeather service', function () {
+		scope.showWeather(1);
+		expect(scope.data.weathers).toEqual([1]);
+	});
+
+	it('should populate error data on unsuccessful call of openWeather service', function () {
+		scope.showError(1);
+		expect(scope.data.isError).toBe(true);
+		expect(scope.data.errors).toEqual([1]);
+	});
 });
--- test/spec/services/openweather.js
+++ test/spec/services/openweather.js
@@ -2,17 +2,42 @@
 
 describe('Service: openWeather', function () {
 
-  // load the service's module
-  beforeEach(module('weatherappApp'));
+	// load the service's module
+	beforeEach(module('weatherappApp'));
 
-  // instantiate service
-  var openWeather;
-  beforeEach(inject(function (_openWeather_) {
-    openWeather = _openWeather_;
-  }));
+	// instantiate service
+	var openWeather;
+	var httpBackend;
 
-  it('should do something', function () {
-    expect(!!openWeather).toBe(true);
-  });
+	beforeEach(inject(function (_openWeather_, $httpBackend) {
+		openWeather = _openWeather_;
+		httpBackend = $httpBackend;
+	}));
+
+	it('should return data for a correctly spelled city name', function () {
+		var ldata;
+		var callback = function (data) {
+			ldata = data;
+		};
+
+		var returnedCity = {
+			city: {
+				id: 1712808,
+				name: 'Goa'
+			}
+		};
+
+		httpBackend.expect('GET', 'http://api.openweathermap.org/data/2.5/forecast/daily?cnt=14&APPID=ce3d05d0909ed5c72c484d06ae442e99&q=goa')
+			.respond(200, [returnedCity]);
+
+		openWeather.getWeather('goa', callback, null);
+		httpBackend.flush();
+		expect(ldata.city.name).toBe('Goab');
+	});
+
+	//  it('should do something', function () {
+	//    expect(!!openWeather).toBe(true);
+	//    //{"message":"","cod":"404"}
+	//  });
 
 });
